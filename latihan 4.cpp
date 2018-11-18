066c171c390f6342eb462d6579d00629be05fc6a
rabkarun <44330354+rabkarun@users.noreply.github.com>
2018-11-18 09:42:33 +0700

    Add files via upload

---
+++ latihan 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+ int n;
+
+ cout << "masukan nilai n: ";
+ cin >> n;
+
+ int (n>50);
+ {
+    (n=n-25);
+    (n=n+10);
+ }
+
+ cout << n;
+}
